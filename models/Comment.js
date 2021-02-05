e6e3f6afc7cbac63e577f1f26035f4c50c2736ca
AnjinIsmail <anjin.ismail@gmail.com>
2021-02-05 14:44:03 -0800

    validation

--- models/Comment.js
+++ models/Comment.js
@@ -4,23 +4,24 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
     {
-        //set custom id to avoid confusion with parent comment _id
+        // set custom id to avoid confusion with parent comment _id
         replyId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId()
         },
         replyBody: {
-            type: String
+            type: String,
+            required: true
         },
         writtenBy: {
-            type: String
+            type: String,
+            required: true,
         },
         createdAt: {
             type: Date,
             default: Date.now,
             get: createdAtVal => dateFormat(createdAtVal)
-        },
-
+        }
     },
     {
         toJSON: {
